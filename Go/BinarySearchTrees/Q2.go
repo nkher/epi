107e6df491dc7be9cbafdafffaa4ab2204a843a8
Namesh Kher <namesh.kher@gmail.com>
2016-01-20 00:26:56 -0500

    Added answers for question 2 and 3 for BST chapter - GO

---
+++ Go/BinarySearchTrees/Q2.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "fmt"
+    "../Structures"
+)
+
+func main() {
+
+    var root *bst.BstNode
+    root = bst.Insert(root, 500)
+    root = bst.Insert(root, 200)
+    root = bst.Insert(root, 100)
+    root = bst.Insert(root, 100)
+    root = bst.Insert(root, 50)
+    root = bst.Insert(root, 250)
+    root = bst.Insert(root, 300)
+    root = bst.Insert(root, 900)
+    root = bst.Insert(root, 1000)
+    root = bst.Insert(root, 1000)
+    root = bst.Insert(root, 700)
+    root = bst.Insert(root, 800)
+
+    ans := findFirstOccurence(root, 100)
+
+    fmt.Println(ans.Data, ans.Right.Data)
+}
+
+func findFirstOccurence(node *bst.BstNode, key int) *bst.BstNode {
+    if node == nil {
+      return nil
+    } else if key == node.Data {
+      leftnode := findFirstOccurence(node.Left, key)
+      if leftnode == nil {
+        return node
+      }
+    }
+    if node.Data < key {
+      return findFirstOccurence(node.Right, key)
+    }
+    return findFirstOccurence(node.Left, key)
+}
---
+++ Go/BinarySearchTrees/Q3.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "../Structures"
+)
+
+func main() {
+
+    var root *bst.BstNode
+    root = bst.Insert(root, 500)
+    root = bst.Insert(root, 200)
+    root = bst.Insert(root, 100)
+    root = bst.Insert(root, 50)
+    root = bst.Insert(root, 250)
+    root = bst.Insert(root, 300)
+    root = bst.Insert(root, 900)
+    root = bst.Insert(root, 1000)
+    root = bst.Insert(root, 1000)
+    root = bst.Insert(root, 700)
+    root = bst.Insert(root, 800)
+
+    firstGreater := findFirstLargerKey(root, 500)
+    fmt.Println("First Greater is : ",  firstGreater.Data)
+}
+
+func findFirstLargerKey(node *bst.BstNode, key int) *bst.BstNode {
+    var firstSoFar, curr *bst.BstNode = nil, node
+
+    for curr != nil {
+      if curr.Data > key {
+        firstSoFar = curr
+        curr = curr.Left
+      } else {
+        curr = curr.Right
+      }
+    }
+    return firstSoFar
+}
