3627a8544541a7cd80b2b1db855d69d7c3edf47b
nkher <namesh.kher@gmail.com>
2018-01-11 20:54:57 -0800

    adding comment

--- Hashmaps/Q1.go
+++ Hashmaps/Q1.go
@@ -1,42 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "../Helper"
+	"fmt"
 )
 
 func main() {
 
-    var list = []string{"debitcard", "silent", "elvis", "badcredit", "lives", "freedom", "listen", "levis"}
-    result := FindAnagramGroups(list)
+	var list = []string{"debitcard", "silent", "elvis", "badcredit", "lives", "freedom", "listen", "levis"}
+	result := FindAnagramGroups(list)
 
-    for i := range result {
-        fmt.Println(result[i])
-    }
+	for i := range result {
+		fmt.Println(result[i])
+	}
 }
 
 func FindAnagramGroups(list []string) [][]string {
 
-    result := [][]string{}
-    hashmap := make(map[string][]string) // hashmap from string to list of strings
+	result := [][]string{}
+	hashmap := make(map[string][]string) // hashmap from string to list of strings
 
-    for i := range list {
-        str := list[i]
-        sortedStr := stringutils.SortString(str)
-        val, ok := hashmap[sortedStr];
-        if ok {
-            val = append(val, str)
-            hashmap[sortedStr] = val
-         } else {
-            var sublist = []string{}
-            sublist = append(sublist, str)
-            hashmap[sortedStr] = sublist
-         }
-    }
-    // doing the grouping here
-    for k, v := range hashmap {
-        fmt.Printf("%v: %v\n", k, v)
-        result = append(result, v)
-    }
-    return result
+	for i := range list {
+		str := list[i]
+		sortedStr := stringutils.SortString(str)
+		val, ok := hashmap[sortedStr]
+		if ok {
+			val = append(val, str)
+			hashmap[sortedStr] = val
+		} else {
+			var sublist = []string{}
+			sublist = append(sublist, str)
+			hashmap[sortedStr] = sublist
+		}
+	}
+	// doing the grouping here
+	for k, v := range hashmap {
+		fmt.Printf("%v: %v\n", k, v)
+		result = append(result, v)
+	}
+	return result
 }
+
+// Q1
